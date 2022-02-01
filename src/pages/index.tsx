d1124dc8972351381fc1ca0576673b42f0fc3964
lihqi <455711093@qq.com>
2022-02-01 11:47:38 +0000

    添加 demo 【无BUG】

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
-import styles from './index.less';
+import useUrlState from '@ahooksjs/use-url-state';
 
-export default function IndexPage() {
+import { Input } from 'antd';
+
+const initialState = {
+  search: '',
+  page: 1,
+  size: 10,
+};
+const App = () => {
+  const [state, setState] = useUrlState({ ...initialState });
+  console.log(JSON.stringify(state));
+
   return (
-    <div>
-      <h1 className={styles.title}>Page index</h1>
-    </div>
+    <Input
+      allowClear={true}
+      placeholder="why"
+      onPressEnter={(e) => {
+        const val = e.currentTarget.value;
+
+        setState({
+          search: val,
+        });
+      }}
+    />
   );
-}
+};
+
+export default App;
